1fc55ad347e752109ad632f17d692e3c1674e287
Khalid Mohammed <khalarasta104@gmail.com>
2023-08-09 02:13:48 +0300

    Add FileStorage class for object storage and retrieval

---
+++ models/engine/file_storage.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+"""
+Defines the FileStorage class.
+"""
+
+import json
+
+
+class FileStorage:
+    """
+    The FileStorage class manages the storage and retrieval of objects in
+    a file.
+
+    Attributes:
+        __file_path (str): The file path to store the objects.
+        __objects (dict): A dictionary to store the objects.
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """
+        Retrieves all objects in the file storage.
+
+        Returns:
+            A dictionary containing all the objects in the file storage.
+        """
+        return self.__objects
+
+    def new(self, obj):
+        """
+        Stores the obj in the __objects dictionary with a key in the format
+        of '<obj class name>.id'
+
+        Args:
+            obj (object): The object to be stored in the __objects dictionary.
+        """
+        key_name = f"{obj.__class__.__name__}.{obj.id}"
+        self.__objects[key_name] = obj
+
+    def save(self):
+        """
+        Saves the objects in the FileStorage instance to a file.
+        """
+        with open(self.__file_path, 'w', encoding='utf8') as file:
+            new_dict = {key: values.to_dict() for key, values in
+                        self.__objects.items()}
+            file.write(json.dumps(new_dict))
+
+    def reload(self):
+        """
+        Reloads the file storage by reading the JSON file and instantiating
+        objects.
+        """
+        from models.base_model import BaseModel
+        class_dict = {
+            "BaseModel": BaseModel
+            # insert other classes here
+        }
+
+        try:
+            with open(self.__file_path, 'r', encoding='utf8') as file:
+                file_data = json.load(file)
+                for obj in file_data.values():
+                    cls_name = obj["__class__"]
+                    del obj["__class__"]
+                    if cls_name in class_dict:
+                        self.new(class_dict[cls_name](**obj))
+        except FileNotFoundError:
+            return
+
+
+
