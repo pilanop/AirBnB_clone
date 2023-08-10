842b65591d5d67ebe46818a4979e6f685d77c9fa
Khalid Mohammed <khalarasta104@gmail.com>
2023-08-10 23:04:39 +0300

    Add 'create' command to console.py

--- console.py
+++ console.py
@@ -1,4 +1,10 @@
 import cmd
+from models.base_model import BaseModel
+
+class_dict = {
+    "BaseModel": BaseModel
+    # insert other classes here
+}
 
 
 class HBNBCommand(cmd.Cmd):
@@ -11,6 +17,17 @@ class HBNBCommand(cmd.Cmd):
     def do_EOF(self, arg):
         """End-of-File (EOF) signal """
         return True
+
+    def do_create(self, arg):
+        """Creates a new instance of a given class"""
+        if not arg:
+            print("** class name missing **")
+        elif arg not in class_dict:
+            print("** class doesn't exist **")
+        else:
+            new_instance = class_dict[arg]()
+            new_instance.save()
+            print(new_instance.id)
 
 
 if __name__ == '__main__':
