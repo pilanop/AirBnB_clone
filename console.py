c57103267fa78922fee0d5a31bc535f6cd6527a3
PAULOS LENDADO <122981444+Pauloslemma@users.noreply.github.com>
2023-08-12 20:14:13 +0300

    for checker

--- console.py
+++ console.py
@@ -1,24 +1,16 @@
 import cmd
-import json
-
-from models import storage
 from models.base_model import BaseModel
-
-class_dict = {
-    "BaseModel": BaseModel
-    # insert other classes here
-}
-
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
 
-    def do_quit(self):
+    def do_quit(self, arg):
         """Quit command to exit the program"""
         return True
 
-    def do_EOF(self):
+    def do_EOF(self, arg):
         """End-of-File (EOF) signal"""
+        print("")
         return True
 
 
